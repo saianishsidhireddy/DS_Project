e6f8762f99eefaada06841d834d3f8790ef5956d
S Sai Anish <aneeshsidhireddy179@gmail.com>
2023-06-21 13:00:46 +0530

    moved the backend files to Django project

--- Facial_emotion_recognition/Backend/src/testing.py
+++ Facial_emotion_recognition/Backend/src/testing.py
@@ -26,7 +26,7 @@ emotion_model.add(Flatten())
 emotion_model.add(Dense(1024, activation='relu'))
 emotion_model.add(Dropout(0.5))
 emotion_model.add(Dense(7, activation='softmax'))
-emotion_model.load_weights('model.h5')
+emotion_model.load_weights('Backend\model.h5')
 cv2.ocl.setUseOpenCL(False)
 emotion_dict = {0: "   Angry   ", 1: "Disgusted", 2: "  Fearful  ", 3: "   Happy   ", 4: "  Neutral  ", 5: "    Sad    ", 6: "Surprised"}
 
--- Facial_emotion_recognition/Detection/views.py
+++ Facial_emotion_recognition/Detection/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.core.files.storage import default_storage
+from .models import CapturedImage
+from .Backend import predict_emotion
 
-# Create your views here.
+
+def predict_emotion_view(request):
+    if request.method == 'POST':
+        uploaded_image = request.FILES['image']
+        image_path = default_storage.save('temp/' + uploaded_image.name, uploaded_image)
+        recognized_emotion = predict_emotion(image_path)  # Implement this function using your model
+        return render(request, 'result.html', {'recognized_emotion': recognized_emotion})
+    
+    return render(request, 'upload.html')
